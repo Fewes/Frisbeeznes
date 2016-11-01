3c59d766ab2e2bafc9295c41aa6ac386cf23621c
Erik Akesson <Erik Akesson>
2016-11-01 15:32:41 +0100

    I have reset everything to ground basic. In app.js we need to choos a start page

--- app/app.js
+++ app/app.js
@@ -3,11 +3,9 @@
 // Declare app level module which depends on views, and components
 angular.module('myApp', [
   'ngRoute',
-  'myApp.weather',
-  'myApp.weatherService',
 ]).
 config(['$locationProvider', '$routeProvider', function($locationProvider, $routeProvider) {
   $locationProvider.hashPrefix('!');
 
-  $routeProvider.otherwise({redirectTo: '/weather'});
+  $routeProvider.otherwise({redirectTo: '/Here wee need to write stuff'}); //<--- write the page we want to start on
 }]);
